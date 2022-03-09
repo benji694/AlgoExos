7d699b0e178b9983f4f5573adc8ff1e337ed72d0
benji694 <bbouillard@edenschool.fr>
2022-03-09 18:05:48 +0100

    fonction nombres aleatoires qui ne se répètent pas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('hello')
+let result = [];
+
+function nbAleatoire(){
+    while(result.length < 15){
+        let nb = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
+        if(result.indexOf(nb) == -1){
+            result.push(nb);
+        }
+    }
+    console.log(result);
+}
+nbAleatoire();
